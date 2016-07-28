d95b1440326592e12097379d56b0f9763e5bc4d2
Colin Osborn <colin.osborn@gmail.com>
2016-07-27 23:19:27 -0600

    ffffff-effin finally

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env["omniauth.auth"])
     if user
-      session[:user_id] = user.user_id
+      session[:user_id] = user[:user_id]
       redirect_to root_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,24 @@ class User < ApplicationRecord
   def self.find_or_create_from_auth(auth)
     user = User.find_or_create_by(provider: auth[:provider], user_id: auth[:uid])
     user_hash(user, auth)
-    byebug
     user.save
     user
   end
 
   def self.user_hash(user, auth)
-    # user.token =            auth[:credentials][:token]
-    user[:firstname] =        auth[:info][:first_name]
-    user[:lastname] =         auth[:info][:last_name]
-    user[:email] =            auth[:info][:email]
-    user[:profile_picture] =  auth[:info][:profile_picture]
-    user[:city] =             auth[:extra][:raw_info][:city]
-    user[:state] =            auth[:extra][:raw_info][:state]
-    user[:sex] =              auth[:extra][:raw_info][:sex]
-    user[:height] =           auth[:info][:height]
-    user[:weight] =           auth[:info][:weight]
-    user[:ftp] =              auth[:extra][:raw_info][:ftp]
-    user[:bikes] =            auth[:info][:bikes]
+    user[:token] =             auth[:credentials][:token]
+    user[:provider] =          auth[:provider]
+    user[:name] =              auth[:info][:name]
+    user[:firstname] =         auth[:info][:first_name]
+    user[:lastname] =          auth[:info][:last_name]
+    user[:email] =             auth[:info][:email]
+    user[:profile_picture] =   auth[:info][:profile]
+    user[:city] =              auth[:extra][:raw_info][:city]
+    user[:state] =             auth[:extra][:raw_info][:state]
+    user[:sex] =               auth[:extra][:raw_info][:sex]
+    user[:weight] =            auth[:extra][:raw_info][:weight]
+    user[:ftp] =               auth[:extra][:raw_info][:ftp]
+    user[:bikes] =             auth[:info][:bikes]
   end
 
 end

0fb02032c36f213c68210c988faf01dd0ee46b76
Colin Osborn <colin.osborn@gmail.com>
2016-08-02 20:30:00 -0600

    adding test coverage for friends

--- spec/integration/user_logs_in_and_sees_their_info_on_dashboard_spec.rb
+++ spec/integration/user_logs_in_and_sees_their_info_on_dashboard_spec.rb
@@ -17,9 +17,11 @@ RSpec.feature 'user logs in an sees their info on the dashboard' do
       expect(page).to have_content("Welcome, #{user.name}")
       expect(page).to have_content("Athlete Stats")
   end
-  scenario "user logs into account to see their basic info" do
+  scenario "user logs into account to see their friends/followers basic info" do
     OmniAuth.config.mock_auth[:strava]
     user = FactoryGirl.create(:user)
+    user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
 
     visit root_path
 
@@ -37,5 +39,17 @@ RSpec.feature 'user logs in an sees their info on the dashboard' do
     click_on "Friends"
     expect(current_path).to eq(athletes_path)
     expect(page).to have_content("Friends Stats")
+    expect(page).to have_content("#{user1.name}")
+    expect(page).to have_content("#{user1.city}")
+    expect(page).to have_content("#{user1.state}")
+    expect(page).to have_content("#{user1.ftp}")
+    expect(page).to have_content("#{user1.bikes}")
+    expect(page).to have_content("#{user1.weight}")
+    expect(page).to have_content("#{user2.name}")
+    expect(page).to have_content("#{user2.city}")
+    expect(page).to have_content("#{user2.state}")
+    expect(page).to have_content("#{user2.ftp}")
+    expect(page).to have_content("#{user2.weight}")
+    expect(page).to have_content("#{user2.bikes}")
   end
 end

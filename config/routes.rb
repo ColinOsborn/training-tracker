a75167ea69102b5774bce8fd4efc4a1a51b9e726
Colin Osborn <colin.osborn@gmail.com>
2016-08-03 01:33:20 -0600

    making change over for athlete comparision

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: "sessions#create", as: :strava_login
   delete "/logout",               to: "sessions#destroy"
   get '/dashboard',               to: "dashboard#show"
-  # resources :dashboard, only: [:show, :index]
   resources :athletes, only: [:show, :index]
 end

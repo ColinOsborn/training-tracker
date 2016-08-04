8c0d2650ba9fa8e7336bc6ebe9f1b7bc19fe2cf6
Colin Osborn <colin.osborn@gmail.com>
2016-08-04 03:54:32 -0600

    bare bones base functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: "sessions#create", as: :strava_login
   delete "/logout",               to: "sessions#destroy"
   get '/dashboard',               to: "dashboard#show"
+  get '/definition',              to: "definition#index"
   resources :athletes, only: [:show, :index]
   resources :goals
   resources :notifications, only: [:create]

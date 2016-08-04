ea22c02941536cc6379ab4d27f70c0a5742e7f47
Colin Osborn <colin.osborn@gmail.com>
2016-08-04 10:30:10 -0600

    tests

--- spec/integration/user_goals_spec.rb
+++ spec/integration/user_goals_spec.rb
@@ -63,4 +63,41 @@ RSpec.feature 'user logs in and sees their goals on dashboard' do
       expect(page).to have_content("#{goal.miles}")
       expect(page).to have_content("#{goal.description}")
       end
+
+      scenario "user can create a goal" do
+        OmniAuth.config.mock_auth[:strava]
+        user = FactoryGirl.create(:user)
+        goal = FactoryGirl.create(:goal)
+
+        visit root_path
+
+        expect(page).to have_content("Login with Strava")
+
+        mock_omniauth
+
+        click_on "Login with Strava"
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_content("Logout")
+        expect(page).to have_content(user.name)
+        expect(page).to have_content("Athlete Stats")
+        expect(page).to have_content("Friends & Followers")
+        expect(page).to have_content("Goals")
+        expect(page).to have_content("Rides")
+
+        click_on "Mileage Goals"
+
+        click_on "#{goal.title}"
+        expect(current_path).to eq(goal_path(goal.id))
+        click_on "Add Another Goal"
+
+        expect(current_path).to eq(new_goal_path)
+        within("#new_goal") do
+          fill_in "Title", with: "Big Miles"
+          fill_in "Miles", with: "300"
+          fill_in "Description", with: "New weekly goal"
+
+        end
+
+        click_on "Create Goal"
     end
+  end
